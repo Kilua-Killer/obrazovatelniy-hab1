a3f94680ec3e2a051f0153670b44030c5995b41f
Kilua-Killer <akmatovzver@gmail.com>
2026-02-04 11:30:08 +1100

    Delete render_deploy directory

--- render_deploy/email-config.js
+++
@@ -1,7 +0,0 @@
-// Email настройки для уведомлений
-const EMAIL_CONFIG = {
-  user: 'obrazovatelniyhab@yandex.ru',
-  pass: 'your_password_here', // Замените на реальный пароль приложения Yandex
-  host: 'smtp.yandex.ru',
-  port: 465
-};
